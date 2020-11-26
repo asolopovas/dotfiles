a46db9353c588f528d74dfefdd6619cfa4726075
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-11-26 16:00:48 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,15 @@ if test -n "$DESKTOP_SESSION"
 end
 
 
-fish_vi_key_bindings
+# fish_vi_key_bindings
+
+fish_hybrid_key_bindings
 
 # Add user scripts
-set -U fish_user_paths ~/.local/bin $fish_user_paths
-set -U fish_user_paths ~/.local/bin $fish_user_paths
-set -U fish_user_paths ~/.local/share/gem/bin $fish_user_paths
-set -U fish_user_paths ~/.config/composer/vendor/bin $fish_user_paths
-set -U fish_user_paths ~/.local/yarn/bin $fish_user_paths
-set -U fish_user_paths ~/.yarn/bin $fish_user_paths
-set -U fish_user_paths ~/.config/fzf/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin                  $fish_user_paths
+set -U fish_user_paths $HOME/.local/share/gem/bin        $fish_user_paths
+set -U fish_user_paths $HOME/.config/composer/vendor/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/yarn/bin             $fish_user_paths
+set -U fish_user_paths $HOME/.yarn/bin                   $fish_user_paths
+set -U fish_user_paths $HOME/.config/fzf/bin             $fish_user_paths
+
