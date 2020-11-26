bc6ea3aba5892943710c741d572cbd9e8b9b8df2
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-11-26 19:42:53 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,10 @@
-set -U fish_prompt_pwd_dir_length 0
-set fish_color_search_match --background=blue
-
+#!/bin/fish
 
 if test -n "$DESKTOP_SESSION"
     set -x (gnome-keyring-daemon --start --components=ssh | string split "=")
 end
 
-
 # fish_vi_key_bindings
-
 fish_hybrid_key_bindings
 
 # Add user scripts
@@ -19,3 +15,11 @@ set -U fish_user_paths $HOME/.local/yarn/bin             $fish_user_paths
 set -U fish_user_paths $HOME/.yarn/bin                   $fish_user_paths
 set -U fish_user_paths $HOME/.config/fzf/bin             $fish_user_paths
 
+# Display full path
+set -U fish_prompt_pwd_dir_length 0
+
+# Change selection background
+set fish_color_search_match --background=blue
+
+set -x TERMINAL alacritty
+set -x EDITOR nvim
