c04b4f82fe158e6173a28f5d9400714ebaac69ec
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-11-26 15:48:27 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,19 @@
 set -U fish_prompt_pwd_dir_length 0
+set fish_color_search_match --background=blue
+
 
 if test -n "$DESKTOP_SESSION"
     set -x (gnome-keyring-daemon --start --components=ssh | string split "=")
 end
+
+
+fish_vi_key_bindings
+
+# Add user scripts
+set -U fish_user_paths ~/.local/bin $fish_user_paths
+set -U fish_user_paths ~/.local/bin $fish_user_paths
+set -U fish_user_paths ~/.local/share/gem/bin $fish_user_paths
+set -U fish_user_paths ~/.config/composer/vendor/bin $fish_user_paths
+set -U fish_user_paths ~/.local/yarn/bin $fish_user_paths
+set -U fish_user_paths ~/.yarn/bin $fish_user_paths
+set -U fish_user_paths ~/.config/fzf/bin $fish_user_paths
