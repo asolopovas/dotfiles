1f5b390e92b4100b0a94b8c299c4489e14d1adaa
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-12-01 20:39:04 +0000

    save

--- .config/fish/functions/rs.fish
+++ .config/fish/functions/rs.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function rs --wraps='rsync -zrvhP ' --description 'alias rs rsync -zrvhP '
+function rs --wraps='rsync -zrvhP ' --description 'alias rs=rsync -zrvhP '
   rsync -zrvhP  $argv;
 end
