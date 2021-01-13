2c46f0069156bfbca3be5a960f6e93984103e500
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-01-13 16:33:09 +0000

    save

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,15 +15,13 @@ if type "xrandr"; then
   xrandr --query | grep " connected" |	while read -r line; do
   primary=$(echo $line | awk '{print$3}')
   display=$(echo $line | awk '{print$1}')
+  user=$(whoami)
 
-
-  if [ "$primary" == "primary" ]; then 
-    if [ $display == 'eDP-1' ]; then
-      CPU_TEMP=$cpu_temp	NETWORK_INTERFACE=$net_interface MONITOR=$display polybar --reload laptop > "/tmp/polybar-laptop.log" 2>&1 &
-    else 
-      echo "loading main bar...\n"
-      CPU_TEMP=$cpu_temp	NETWORK_INTERFACE=$net_interface MONITOR=$display polybar --reload main > "/tmp/polybar-main.log" 2>&1 &
-    fi
+  if [ "$primary" == "primary" ] && [ $display == 'eDP-1' ]; then 
+    CPU_TEMP=$cpu_temp	NETWORK_INTERFACE=$net_interface MONITOR=$display polybar --reload laptop > "/tmp/$user-polybar-laptop.log" 2>&1 &
+  else 
+    echo "loading main bar...\n"
+    CPU_TEMP=$cpu_temp	NETWORK_INTERFACE=$net_interface MONITOR=$display polybar --reload main > "/tmp/$user-polybar-main.log" 2>&1 &
   fi
 done
 fi
