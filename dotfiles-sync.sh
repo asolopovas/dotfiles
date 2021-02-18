3cbccb3de31fab660a3cfab1c6d42c3bf072bb6e
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-02-18 14:24:18 +0000

    save

---
+++ dotfiles-sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function syncConfig {
+  srcPath=$1
+  destPath=$2
+
+  readarray -d '' dotfiles < <(find $srcPath -maxdepth 1 )
+
+  for src in $dotfiles
+  do
+    if [  $srcPath == $src ]; then continue; fi
+    dest=${src/\/dotfiles\///}
+    rm -rf $dest
+    ln -sf $src $destPath
+  done
+}
+
+syncConfig ~/dotfiles/.config ~/.config
+syncConfig ~/dotfiles/.local ~/.local
+
+
+# Sync dotfile from root directory
+readarray -d '' dotfiles < <(find ~/dotfiles -maxdepth 1 -type f  )
+for src in $dotfiles
+do
+  dest=${src/\/dotfiles\///}
+  ln -sf $src $dest
+done
+
+# install plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
