327e5090e458291e5110fe024ed6d3e5c4489893
Andrius Solopovas <andrius.solopovas@gmail.com>
2021-02-18 15:27:15 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,8 @@ fish_default_key_bindings
 set fish_greeting
 set fish_color_search_match --background=blue
 set -U fish_prompt_pwd_dir_length 0
+set -U FZF_FIND_FILE_COMMAND 'fd -E node_modules -E Steam -E npm -E skype -E vscode -E Code -E chrome -E Cache -E cache -E gem -E fish -E python -E .git -E mozilla -E cargo -E log . $HOME'
+set -U FZF_CD_COMMAND 'fd -t d -E node_modules -E Steam -E npm -E skype -E vscode -E Code -E chrome -E Cache -E cache -E gem -E fish -E python -E .git -E mozilla -E cargo -E log . $HOME'
 
 # ----------------------------------
 # Env Variables
