881e6fb7accc1e6cbcabf6f518e22c1a4eabb8a5
andrius <andrius.solopovas@gmail.com>
2023-08-12 16:14:16 +0100

    save

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-
+gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
 .config/polybar/launch.sh &
 nohup cryptomator &
 flameshot &
 nm-applet &
-blueman-applet &
+blueman-appletk
 setbg &
 insync start
+
