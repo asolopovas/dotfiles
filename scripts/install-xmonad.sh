f03aeff84567bfebfc50fc0d9c4e2860f1e4499d
andrius <andrius.solopovas@gmail.com>
2023-08-26 23:10:45 +0100

    save

--- scripts/install-xmonad.sh
+++ scripts/install-xmonad.sh
@@ -1,43 +1,67 @@
 #!/bin/bash
 
+# Functions
+install_packages() {
+    local -A install_cmds=(
+        ["debian"]="sudo apt install git libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxss-dev"
+        ["fedora"]="sudo dnf install git libX11-devel libXft-devel libXinerama-devel libXrandr-devel libXScrnSaver-devel"
+        ["arch"]="sudo pacman -S git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf"
+    )
+    ${install_cmds["$1"]}
+}
+
+setup_xmonad_log() {
+    mkdir -p "$XMONAD_LOG"
+    git clone https://github.com/xintron/xmonad-log.git "$XMONAD_LOG"
+    go get github.com/godbus/dbus
+    go mod init
+    go build
+    go install
+}
+
+clone_repo_if_not_exists() {
+    [ ! -d "$2" ] && git clone --branch "$3" "$1" "$2"
+}
+
+# Variables
+OS=$(awk -F= '/^ID=/ {gsub(/"/, "", $2); print tolower($2)}' /etc/os-release)
 XMONAD_ORIGIN="$HOME/dotfiles/.config/xmonad"
 XMONAD_DEST="$HOME/.config/xmonad"
 XMONAD_LOG="$GOPATH/src/github.com/xintron"
 
+echo $OS
+
 # Installing dependencies for different systems
-if [ -f /etc/debian_version ]; then
-    sudo apt install git libx11-dev libxft-dev libxinerama-dev libxrandr-dev libxss-dev
+if [ -f /etc/debian_version ] || [ "$OS" == "pop" ]; then
+    install_packages "debian"
 elif [ -f /etc/fedora-release ]; then
-    sudo dnf install git libX11-devel libXft-devel libXinerama-devel libXrandr-devel libXScrnSaver-devel
+    install_packages "fedora"
 elif [ -f /etc/arch-release ]; then
-    sudo pacman -S git xorg-server xorg-apps xorg-xinit xorg-xmessage libx11 libxft libxinerama libxrandr libxss pkgconf
+    install_packages "arch"
 else
     echo "Unsupported system."
     exit 1
 fi
 
+# Install Haskell Stack if not present
 command -v stack &>/dev/null || curl -sSL https://get.haskellstack.org/ | sh
 
-[ ! -d $XMONAD_DEST ] && mkdir -p $XMONAD_DEST
-
+# Set up Xmonad config
+mkdir -p "$XMONAD_DEST"
 ln -sf "$XMONAD_ORIGIN/xmonad.hs" "$XMONAD_DEST/xmonad.hs" >/dev/null 2>&1
 ln -sf "$XMONAD_ORIGIN/stack.yaml" "$XMONAD_DEST/stack.yaml" >/dev/null 2>&1
 
-if [ ! -d $XMONAD_LOG ]; then
-    mkdir -p $XMONAD_LOG
-    git clone https://github.com/xintron/xmonad-log.git $XMONAD_LOG
-    go get github.com/godbus/dbus
-    go mod init
-    go build
-    go install
-fi
+# Setup xmonad-log if not exists
+[ ! -d $XMONAD_LOG ] && setup_xmonad_log
 
-[ ! -d $XMONAD_DEST/xmonad ] && git clone --branch v0.17.2 https://github.com/xmonad/xmonad $XMONAD_DEST/xmonad
-[ ! -d $XMONAD_DEST/xmonad-contrib ] && git clone --branch v0.17.1 https://github.com/xmonad/xmonad-contrib $XMONAD_DEST/xmonad-contrib
-[ ! -d $XMONAD_DEST/xmonad-dbus ] && git clone https://github.com/troydm/xmonad-dbus.git $XMONAD_DEST/xmonad-dbus
+# Clone repositories if not present
+clone_repo_if_not_exists "https://github.com/xmonad/xmonad" "$XMONAD_DEST/xmonad" "v0.17.2"
+clone_repo_if_not_exists "https://github.com/xmonad/xmonad-contrib" "$XMONAD_DEST/xmonad-contrib" "v0.17.1"
+clone_repo_if_not_exists "https://github.com/troydm/xmonad-dbus.git" "$XMONAD_DEST/xmonad-dbus" ""
 
 stack install
 
+# Create xmonad.desktop if not exists
 if [ ! -f /usr/share/xsessions/xmonad.desktop ]; then
     sudo tee /usr/share/xsessions/xmonad.desktop >/dev/null <<EOT
 [Desktop Entry]
