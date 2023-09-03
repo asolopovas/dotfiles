9a52840cb78df8b4007ec11984d2b227d5316d03
andrius <andrius.solopovas@gmail.com>
2023-09-03 12:39:10 +0100

    save

--- scripts/install-fd.sh
+++ scripts/install-fd.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+source ./os.sh
+
 if [ "$OS" = "ubuntu" ] || [ ""$OS"" = "debian" ]; then
     print_color green "INSTALLING FD-FIND..."
     curl -fsSLO https://github.com/sharkdp/fd/releases/download/v8.5.3/fd-musl_8.5.3_amd64.deb
     sudo dpkg -i fd-musl_8.5.3_amd64.deb
     rm -rf fd-musl_8.5.3_amd64.deb
+fi
+
+if [ "$OS" = "almalinux" ]; then
+    $VER = "8.7.0"
+    $NAME="fd-v${VER}-x86_64-unknown-linux-gnu"
+    curl -fssLO https://github.com/sharkdp/fd/releases/download/v$VER/$NAME.tar.gz
+    tar -xf $NAME.tar.gz -C . $NAME/fd
+    mv $NAME/fd /usr/local/bin
+    rm -rf $NAME $NAME.tar.gz
 fi
 
 if [ "$OS" = "alpine" ]; then
