892fad944e158134aceaaaae728dc4f26c188610
andrius <andrius.solopovas@gmail.com>
2023-09-13 11:15:21 +0100

    prompt update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,57 +1,59 @@
 # name: default
 # ---------------
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
-end
+# Initialize color settings at the start of the session.
+set -U color_cyan (set_color cyan)
+set -U color_yellow (set_color yellow)
+set -U color_red (set_color red)
+set -U color_blue (set_color blue)
+set -U color_green (set_color green)
+set -U color_brgreen (set_color brgreen)
+set -U color_normal (set_color normal)
 
-function _git_is_dirty
-  echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
+function _git_info
+    set -l branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
+    if [ "$branch" ]
+        set -l dirty (command git status -s --ignore-submodules=dirty 2> /dev/null)
+        if [ "$dirty" ]
+            echo $branch "±"
+        else
+            echo $branch
+        end
+    end
 end
 
 function fish_prompt
-  set -l last_status $status
+    set -l last_status $status
+    set -l user (whoami)
+    set -l cwd $color_blue(pwd | sed "s:^$HOME:~:")
 
-  set -l cyan (set_color cyan)
-  set -l yellow (set_color yellow)
-  set -l red (set_color red)
-  set -l blue (set_color blue)
-  set -l green (set_color green)
-  set -l brgreen (set_color brgreen)
-  set -l normal (set_color normal)
-  set -l user (whoami)
-
-  set -l cwd $blue(pwd | sed "s:^$HOME:~:")
-
-  # Display [venvname] if in a virtualenv
-  if set -q VIRTUAL_ENV
-      echo -n -s (set_color -b cyan black) '[' (basename "$VIRTUAL_ENV") ']' $normal ' '
-  end
-
-  # Print pwd or full path
-  echo -n -s $cwd $normal
-
-  # Show git branch and status
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name)
-
-    if [ (_git_is_dirty) ]
-      set git_info $yellow $git_branch "±" $normal
-    else
-      set git_info $green $git_branch $normal
+    # Display [venvname] if in a virtualenv
+    if set -q VIRTUAL_ENV
+        echo -n -s $color_cyan '[' (basename "$VIRTUAL_ENV") ']' $color_normal ' '
     end
-    echo -n -s ' ' $git_info $normal
-  end
 
-  # Display username
-  echo -n -s $brgreen ' ('  $user ')' $normal
+    # Print pwd or full path
+    echo -n -s $cwd $color_normal
 
-  set -l prompt_color $red
-  if test $last_status = 0
-    set prompt_color $normal
-  end
+    # Show git branch and status
+    set -l gitinfo (_git_info)
+    if [ "$gitinfo" ]
+        if echo $gitinfo | grep "±" > /dev/null
+            echo -n -s ' ' $color_yellow $gitinfo $color_normal
+        else
+            echo -n -s ' ' $color_green $gitinfo $color_normal
+        end
+    end
 
-  # Terminate with a nice prompt char
-  echo -e ''
-  echo -e -n -s $prompt_color '> ' $normal
+    # Display username
+    echo -n -s $color_yellow ' [ ' $color_brgreen  $user $color_yellow ' ]' $color_normal
+
+    set -l prompt_color $color_red
+    if test $last_status = 0
+        set prompt_color $color_normal
+    end
+
+    # Terminate with a nice prompt char
+    echo -e ''
+    echo -e -n -s $prompt_color '> ' $color_normal
 end
