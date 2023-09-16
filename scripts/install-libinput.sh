e4d8ff978ff9742d2309b33afca43e24bc9c86a0
andrius <andrius.solopovas@gmail.com>
2023-09-16 15:57:24 +0100

    touchpad settings

--- scripts/install-libinput.sh
+++ scripts/install-libinput.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 
-file="/etc/X11/xorg.conf.d/40-libinput.conf"
+file="/etc/X11/xorg.conf.d/40-touchpad.conf"
 
 sudo tee "$file" >/dev/null <<EOL
 Section "InputClass"
-    Identifier "libinput touchpad catchall"
+    Identifier "SynPS/2 Synaptics TouchPad"
     MatchIsTouchpad "on"
     MatchDevicePath "/dev/input/event*"
     Driver "libinput"
-    MatchIsTouchpad "on"
+    # Tapping
     Option "Tapping" "on"
     Option "TappingDrag" "on"
-    Option "TappingDragLock" "off"
-    Option "TappingButtonMapping" "1 0"
-    Option "NaturalScrolling" "true"
-    Option "DisableWhileTyping" "true"
-    Option "ScrollMethod" "1 0 0"
-    Option "ScrollFactor" "0.5"
-    Option "ClickMethod" "1 0"
-    Option "MiddleEmulation" "off"
-    Option "AccelSpeed" "1.0"
-    Option "AccelProfile" "1 0"
-    Option "LeftHanded" "off"
-    Option "SendEventsMode" "0 0"
-    Option "ScrollingPixelDistance" "15"
-    Option "HorizontalScroll" "on"
+    # Scrolling
+    Option "NaturalScrolling" "on"
+    Option "AccelSpeed" "0.3"
+    Option "ScrollPixelDistance" "25"
 EndSection
 EOL
 
