868f766ac9aae6f6bdedc51190c2e057c52cd4c4
andrius <andrius.solopovas@gmail.com>
2023-09-20 22:51:25 +0100

    add-fstab completion bash

--- completions/bash/add-fstab.sh
+++ completions/bash/add-fstab.sh
@@ -1,10 +1,12 @@
 _add_fstab_completion() {
     local cur_word="${COMP_WORDS[COMP_CWORD]}"
-    local level1=$(ls /dev)
-    local level2="ext4 ntfs vfat xfs btrfs"
+
+    # Get devices and sizes similarly to how the Fish script does
+    local info=$(lsblk -nlo NAME,TYPE,SIZE | awk '/^data-root/ {print "/dev/mapper/" $1; next} /sd.[0-9]|nvme[0-9]n[0-9]p[0-9]/ {print "/dev/" $1}')
+    local level2="ext4 ntfs ntfs-3g vfat xfs btrfs"
 
     if test_depth 1; then
-        COMPREPLY=( $(compgen -W "${level1}" -- "${cur_word}") )
+        COMPREPLY=( $(compgen -W "${info}" -- "${cur_word}") )
     elif test_depth 2; then
         COMPREPLY=( $(compgen -W "${level2}" -- "${cur_word}") )
     fi
