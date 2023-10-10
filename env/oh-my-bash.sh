90cad22ea66d8bd99d1967906b982d7c53de8520
andrius <andrius.solopovas@gmail.com>
2023-10-10 20:13:11 +0100

    oh-my-bash

--- env/oh-my-bash.sh
+++ env/oh-my-bash.sh
@@ -18,12 +18,20 @@ plugins=(
     bashmarks
 )
 
-theme_dir="$HOME/.local/share/ohmybash/custom/themes/theme"
+export ohmybash_dir="$HOME/.local/share/ohmybash"
+theme_dir="$ohmybash_dir/custom/themes/theme"
 theme_file="$theme_dir/theme.theme.sh"
 src_file="$HOME/dotfiles/env/theme.sh"
 
-[ ! -d "$theme_dir" ] || [ ! -f "$theme_file" ]; \
-mkdir -p "$theme_dir" >/dev/null; \
-ln -sf "$src_file" "$theme_file" >/dev/null
+if [ ! -d "$theme_dir" ] || [ ! -f "$theme_file" ]; then
+    mkdir -p "$theme_dir"
+    ln -sf "$src_file" "$theme_file"
+fi
 
-[ -f "$OSH/oh-my-bash.sh" ] && source "$OSH/oh-my-bash.sh"
+if [ ! -d "$ohmybash_dir" ]; then
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended
+    rm -rf "$HOME/.bashrc*"
+    ln -sf "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
+fi
+
+[ -f "$ohmybash_dir/oh-my-bash.sh" ] && source "$ohmybash_dir/oh-my-bash.sh"
