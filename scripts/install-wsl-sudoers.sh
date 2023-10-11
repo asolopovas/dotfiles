aa4e1ca5905921ee8955cd8a8b93d4d452360c77
andrius <andrius.solopovas@gmail.com>
2023-10-11 19:49:19 +0100

    save

---
+++ scripts/install-wsl-sudoers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Requesting elevated privileges..."
+    sudo "$0"  # Re-run this script with sudo
+    exit $?
+fi
+
+# Get the current username
+USER_NAME="${SUDO_USER:-$(whoami)}"
+
+echo 'Defaults !tty_tickets' > /etc/sudoers.d/wsl.sudoers
+
+newStr="${USER_NAME} ALL=(ALL:ALL) NOPASSWD: /usr/sbin/shutdown,/usr/sbin/reboot,/usr/bin/mount,/usr/bin/umount,/usr/bin/apt update -y,/usr/bin/apt upgrade -y"
+echo "${newStr}" >> /etc/sudoers.d/wsl.sudoers
+
+chmod 440 /etc/sudoers.d/wsl.sudoers
