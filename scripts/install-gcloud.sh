7270f512fb03046edeea1176accfefc23ec6a427
andrius <andrius.solopovas@gmail.com>
2023-10-15 15:48:16 +0100

    gp

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-VERSION=${1:-"448.0.0"}
-curl -#O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-$VERSION-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-$VERSION-linux-x86_64.tar.gz
-./google-cloud-sdk/install.sh
-./google-cloud-sdk/bin/gcloud init
+
+snap install google-cloud-cli --classic
