b9c27efb7495b16f1cdbc707ef46426801a3a37e
andrius <andrius.solopovas@gmail.com>
2023-10-18 23:24:11 +0100

    save

--- scripts/install-software.sh
+++ scripts/install-software.sh
@@ -1,94 +1,83 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Requesting elevated privileges..."
+    sudo "$0" "$@" # Run the script as root
+    exit $?
+fi
+
 OS=$(awk '/^ID=/' /etc/os-release | sed -e 's/ID=//' -e 's/"//g' | tr '[:upper:]' '[:lower:]')
+DOTFILES_DIR="$HOME/dotfiles"
+source $DOTFILES_DIR/functions.sh
+
+print_color green "Installing Packages for $OS"
 
 command_exists() {
-  command -v $1 >/dev/null 2>&1
+    command -v $1 >/dev/null 2>&1
 }
 
 is_sudoer() {
-  sudo -v > /dev/null 2>&1
+    sudo -v >/dev/null 2>&1
 }
 
 removePackage() {
-  printf "\n************************************************\n"
-  printf "Removing $1 package from the system\n"
-  printf "\n************************************************\n"
-  if command_exists $1 && is_sudoer; then
-    echo $OS
-    case $OS in
-      ubuntu)
-        sudo apt remove -y $1;
-        ;;
-      centos)
-        sudo yum remove -y $1;
-        ;;
-      arch)
-        sudo pacman -Rns --noconfirm $1;
-        ;;
-    esac
-  fi
+    print_color reed "Removing $1 package"
+    if command_exists $1 && is_sudoer; then
+        case $OS in
+        ubuntu)
+            apt remove -y $1
+            ;;
+        centos)
+            sudo yum remove -y $1
+            ;;
+        arch)
+            sudo pacman -Rns --noconfirm $1
+            ;;
+        esac
+    fi
 }
 
-installPackage() {
-  printf "\n************************************************\n"
-  printf "Installing $1 package\n"
-  printf "\n************************************************\n"
-  if ! command_exists $1 && is_sudoer; then
+installPackages() {
+    print_color green "Installing the following packages:"
+    for pkg in "$@"; do
+        print_color blue "  - $pkg"
+    done
     case $OS in
-      ubuntu)
-        sudo apt install -y $1;
+    ubuntu | debian)
+        sudo apt install -y "$@"
         ;;
-      centos)
-        sudo yum install -y $1;
+    centos)
+        sudo yum install -y "$@"
         ;;
-      arch)
-        sudo pacman -S --noconfirm $1;
+    arch)
+        sudo pacman -S --noconfirm "$@"
         ;;
     esac
-  fi
 }
 
-cd $HOME
+packages=(
+    "fd-find"
+    "fish"
+    "flameshot"
+    "fzf"
+    "htop"
+    "jq"
+    "libgtkglext1"
+    "libguestfs-tools"
+    "libnss3-tools"
+    "lxappearance"
+    "polybar"
+    "ripgrep"
+    "rofi"
+    "snapd"
+    "sqlite3"
+    "stacer"
+    "timeshift"
+    "xwallpaper"
+)
 
-# -------------------------------------
-# Install Fish
-# -------------------------------------
-while true; do
-  read -p "Install Fish Shell? (default: no) [yes/no]" yn
-  case $yn in
-    [Yy]*)
-      installPackage fish
-      curl -L https://get.oh-my.fish | fish
-      fish -c "omf install agnoster; omf theme agnoster"
-      default_shell=$(which fish)
-      break;;
-    *)
-      break;;
-  esac
-done
+[ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ] && sudo apt update
 
+installPackages "${packages[@]}"
 
-# -------------------------------------
-# Install fuzzy search
-# -------------------------------------
-while true; do
-  read -p "Install fuzzy search for terminal? (default: no) [yes/no]" yn
-  case $yn in
-    [Yy]*)
-      installPackage ripgrep
-      installPackage fzf
-      if [ "$OS" == "ubuntu" ]; then
-        installPackage fd-find
-        sudo ln -sf /usr/bin/fdfind /usr/bin/fd
-      fi
-      printf "Installing Fuzzy Search...\n"
-      mkdir -p ~/.local/share/gem/bin
-      git clone --depth 1 https://github.com/junegunn/fzf.git ~/.config/fzf
-      ~/.config/fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
-      mv ~/.fzf.zsh ~/.config/fzf.zsh
-        break;;
-    *)
-      break;;
-  esac
-done
-
+[ "$OS" == "ubuntu" ] || [ "$OS" == "debian" ] && sudo ln -sf /usr/bin/fdfind /usr/bin/fd
