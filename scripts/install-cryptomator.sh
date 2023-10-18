ad105ec476d9b619533b6f7934e386824bd7d8c2
andrius <andrius.solopovas@gmail.com>
2023-10-18 22:40:03 +0100

    save

--- scripts/install-cryptomator.sh
+++ scripts/install-cryptomator.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-URL="https://github.com/cryptomator/cryptomator/releases/download/1.10.1/cryptomator-1.10.1-aarch64.AppImage"
+VER="1.10.1"
+ARCH="x86_64"
+URL="https://github.com/cryptomator/cryptomator/releases/download/${VER}/cryptomator-${VER}-${ARCH}.AppImage"
 DEST="$HOME/.local/bin/cryptomator"
 
 curl -L "$URL" -o "$DEST"
