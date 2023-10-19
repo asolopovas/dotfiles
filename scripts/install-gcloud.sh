ad7c5ff1ff5c92143a29bd8bf1f01847b3813df0
andrius <andrius.solopovas@gmail.com>
2023-10-19 18:24:40 +0100

    save

--- scripts/install-gcloud.sh
+++ scripts/install-gcloud.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-snap install google-cloud-cli --classic
+# Ensure the target directory exists
+mkdir -p "$HOME/src"
+
+# Change to the target directory
+cd "$HOME/src" || exit 1
+
+# Variables
+VER="451.0.0"
+ARCH="x86_64"
+NAME="google-cloud-cli-${VER}-linux-${ARCH}.tar.gz"
+
+# Method 3: Install using a .tar.gz file
+curl -#fsSLO "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/$NAME"
+tar zxvf "$NAME"
+./google-cloud-sdk/install.sh
+rm -rf "$NAME"
