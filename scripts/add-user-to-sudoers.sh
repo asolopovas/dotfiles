11b6ee36471da724d4f4f4b728e0e20507ca3fc9
andrius <andrius.solopovas@gmail.com>
2023-10-19 13:51:40 +0100

    save

--- scripts/add-user-to-sudoers.sh
+++ scripts/add-user-to-sudoers.sh
@@ -12,7 +12,7 @@ USER_NAME="${SUDO_USER:-$(whoami)}"
 
 echo 'Defaults !tty_tickets' > /etc/sudoers.d/${USER_NAME}.sudoers
 
-newStr="${USER_NAME} ALL=(ALL:ALL) NOPASSWD: /usr/sbin/shutdown,/usr/sbin/reboot,/usr/bin/mount,/usr/bin/umount"
+newStr="${USER_NAME} ALL=(ALL:ALL) NOPASSWD: /sbin/reboot,/sbin/shutdown,/usr/sbin/reboot,/usr/sbin/shutdown,/usr/bin/mount,/usr/bin/umount"
 echo "${newStr}" >> /etc/sudoers.d/${USER_NAME}.sudoers
 
 chmod 440 /etc/sudoers.d/${USER_NAME}.sudoers
