c5b5b10c60204c73c0961778200c25976b00fdac
andrius <andrius.solopovas@gmail.com>
2023-10-24 22:02:25 +0100

    save

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,18 +11,4 @@ while pgrep -u $UID -x polybar >$HOME/logs/polybar.log; do sleep 1; done
 
 net_interface=$(ip route show | grep default | awk '{print$5}')
 
-
-# if command -v xrandr >/dev/null 2>&1; then
-#     xrandr --query | grep " connected" | while read -r line; do
-#         primary=$(echo $line | awk '{print$3}')
-#         display=$(echo $line | awk '{print$1}')
-#         user=$(whoami)
-
-#         #   printf "Primary: $primary\n"
-#         #   printf "Display: $display\n"
-#         #   printf "User: $user\n"
-
-#     done
-# fi
-
 CONFIG_DIR=$CONFIG_DIR MONITOR=$display polybar main -c $CONFIG --reload
