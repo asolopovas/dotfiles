5af8a605295b140a31bee1b80ce7e8904c2f491c
andrius <andrius.solopovas@gmail.com>
2023-10-25 13:44:47 +0100

    save

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-xrandr --output DP-2 --primary --mode 1920x1080 --pos 0x0 --output DP-0 --mode 1920x1080 --pos 1920x0
-
 gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
 .config/polybar/launch.sh > /tmp/polybar.log 2>&1 &
 nohup cryptomator > /tmp/cryptomator.log 2>&1 &
