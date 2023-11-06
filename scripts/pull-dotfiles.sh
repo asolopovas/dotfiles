6e0d59f492a2c23a150f6df281d9394a6a00f600
andrius <andrius.solopovas@gmail.com>
2023-11-06 14:33:27 +0000

    save

--- scripts/pull-dotfiles.sh
+++ scripts/pull-dotfiles.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 for userdir in /home/*; do
-    if [ -d "${userdir}/dotfiles" ]; then
-        cd "${userdir}/dotfiles"
-        if [ -d ".git" ]; then
-            git reset --hard
-            git pull
+    dotfiles_dir="${userdir}/dotfiles"
+
+    if [ -d "$dotfiles_dir" ]; then
+        owner=$(stat -c '%U' "$dotfiles_dir")
+        if [ -d "$dotfiles_dir/.git" ]; then
+            sudo -u "$owner" git -C "$dotfiles_dir" reset --hard
+            sudo -u "$owner" git -C "$dotfiles_dir" pull
         else
             echo "'dotfiles' in ${userdir} is not a git repository."
         fi
-        cd - >/dev/null
     else
         echo "No 'dotfiles' directory found in ${userdir}."
     fi
