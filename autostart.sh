cbfd9747ff4d1cc69fc052821fe36558212d21a8
andrius <andrius.solopovas@gmail.com>
2023-11-15 12:39:45 +0000

    save

--- autostart.sh
+++ autostart.sh
@@ -8,5 +8,5 @@ blueman-applet > /tmp/blueman.log 2>&1 &
 set-wallpaper &
 dunst &
 telegram-desktop &
-setxkbmap -layout gb,ru -option 'grp:win_space_toggle'
+# setxkbmap -layout gb,ru -option 'grp:win_space_toggle'
 insync start
