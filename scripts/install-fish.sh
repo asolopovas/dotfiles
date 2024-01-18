850a55c643e9d6206e32b2efd8b39b7181dd1643
andrius <andrius.solopovas@gmail.com>
2024-01-18 18:14:32 +0000

    save

--- scripts/install-fish.sh
+++ scripts/install-fish.sh
@@ -2,20 +2,13 @@
 
 source "$HOME/dotfiles/globals.sh"
 
-configure_fish() {
-    rm -rf "$HOME/.config/fish"
-    ln -sf "$DOTFILES_DIR/.config/fish" "$HOME/.config/"
-}
-
 case $OS in
 ubuntu | debian | linuxmint | pop)
     sudo apt-add-repository -y ppa:fish-shell/release-3
     sudo apt update -qq -y
     sudo apt install fish -y
     ;;
-alpine)
-    configure_fish
-    ;;
 esac
 
-configure_fish
+rm -rf "$HOME/.config/fish"
+ln -sf "$DOTFILES_DIR/.config/fish" "$HOME/.config/"
