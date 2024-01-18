041bb532d0cd026a5954cabdf8c301984a9aeda2
andrius <andrius.solopovas@gmail.com>
2024-01-18 18:18:56 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,11 +34,14 @@ end
 
 if type -q bass
     set nvm_path $HOME/.nvm/nvm.sh
+
     if not test -f $nvm_path
         set nvm_path $HOME/.config/nvm/nvm.sh
     end
 
-    bass source $nvm_path
+    if test -f $nvm_path
+        bass source $nvm_path
+    end
 end
 
 
