eeac4be6039d517d5cb99f3ce5de761f959f91b8
andrius <andrius.solopovas@gmail.com>
2024-03-04 21:56:02 +0000

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -57,3 +57,7 @@ end
 # pnpm end
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f "$HOME/.local/google-cloud-sdk/path.fish.inc" ]; . "$HOME/.local/google-cloud-sdk/path.fish.inc"; end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
