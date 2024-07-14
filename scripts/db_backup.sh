6d7d505186c9e6bd4e8564e4bc304bfba1893b79
andrius <andrius.solopovas@gmail.com>
2024-07-14 10:30:48 +0100

    backups working

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -18,6 +18,32 @@ OUTPUT_DIR=$(pwd)
 # Ensure the output directory exists
 mkdir -p "$OUTPUT_DIR"
 
+# Define the special wp files
+WP_FILES=(
+  "wp_commentmeta"
+  "wp_comments"
+  "wp_options"
+  "wp_postmeta"
+  "wp_posts"
+  "wp_term_relationships"
+  "wp_term_taxonomy"
+  "wp_termmeta"
+  "wp_terms"
+  "wp_usermeta"
+  "wp_users"
+)
+
+# Function to check if a table is in the wp_files array
+is_wp_file() {
+  local table=$1
+  for wp_file in "${WP_FILES[@]}"; do
+    if [ "$wp_file" == "$table" ]; then
+      return 0
+    fi
+  done
+  return 1
+}
+
 # Get the list of tables in the database
 TABLES=$(mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "SHOW TABLES;" | awk '{ print $1}' | grep -v '^Tables' )
 
@@ -26,9 +52,15 @@ for TABLE in $TABLES; do
   ROW_COUNT=$(mariadb -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "SELECT COUNT(*) FROM $TABLE;" | awk 'NR==2')
 
   if [ "$ROW_COUNT" -gt 0 ]; then
-    # Export the table if it has rows
-    mariadb-dump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" "$TABLE" > "$OUTPUT_DIR/$TABLE.sql"
-    echo "Exported $TABLE with $ROW_COUNT rows"
+    if is_wp_file "$TABLE"; then
+      PREFIX_DIR="$OUTPUT_DIR/wp"
+    else
+      TABLE_PREFIX=$(echo $TABLE | cut -d'_' -f1-2)
+      PREFIX_DIR="$OUTPUT_DIR/$TABLE_PREFIX"
+    fi
+    mkdir -p "$PREFIX_DIR"
+    mariadb-dump --no-create-info -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" "$TABLE" > "$PREFIX_DIR/$TABLE.sql"
+    echo "Exported $TABLE with $ROW_COUNT rows into $PREFIX_DIR"
   else
     echo "Skipped $TABLE (no data)"
   fi
