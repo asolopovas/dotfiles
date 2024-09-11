6020771c16c882b51dc80e6bfc82008fb61d7ae6
andrius <andrius.solopovas@gmail.com>
2024-09-11 18:59:15 +0100

    save

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,20 +62,6 @@ if [ -f "$HOME/.local/google-cloud-sdk/path.fish.inc" ]; . "$HOME/.local/google-
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
-# Start ssh-agent if not running and store the environment variables
-if not set --query SSH_AGENT_PID
-    if test -f ~/.ssh-agent-env
-        source ~/.ssh-agent-env
-        if not kill -0 $SSH_AGENT_PID >/dev/null 2>&1
-            echo "Stale agent file, starting new agent"
-            eval (ssh-agent -c | sed 's/^/set -x /') > ~/.ssh-agent-env
-        end
-    else
-        eval (ssh-agent -c | sed 's/^/set -x /') > ~/.ssh-agent-env
-    end
-end
-
-# Add SSH key with a timeout of 12 hours
-if not ssh-add -l >/dev/null 2>&1
-    ssh-add -t 12h ~/.ssh/id_rsa >/dev/null 2>&1
+if not pgrep ssh-agent > /dev/null
+    eval (ssh-agent -c)
 end
