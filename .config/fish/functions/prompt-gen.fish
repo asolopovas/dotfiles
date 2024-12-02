de9bccc9ae3ce7da126dfa5d68409450d4cce190
andrius <andrius.solopovas@gmail.com>
2024-12-02 10:45:08 +0000

    save

--- .config/fish/functions/prompt-gen.fish
+++ .config/fish/functions/prompt-gen.fish
@@ -3,24 +3,23 @@ function prompt-gen
         echo "Usage: prompt-gen <file1> <file2> ..."
         return 1
     end
-    set output_file .prompt.txt
+
     echo "# Follow these important rules
 Do not include comments in the code
 Avoid ending lines with semicolons
-Strive for compactness, but maintain readability" > $output_file
+Strive for compactness, but maintain readability"
 
     for file in $argv
         if test -e $file
-            echo "" >> $output_file
-            echo (basename $file) >> $output_file
+            echo ""
+            echo (basename $file)
             set extension (string match -r '\.\w+$' $file | string sub -s 2)
-            echo '```'(string lower $extension) >> $output_file
-            cat $file >> $output_file
-            echo '```' >> $output_file
-            echo "" >> $output_file
+            echo '```'(string lower $extension)
+            cat $file
+            echo '```'
+            echo ""
         else
             echo "File not found: $file"
         end
     end
-    echo "Combined file created: $output_file"
 end
