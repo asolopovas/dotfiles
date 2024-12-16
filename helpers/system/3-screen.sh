b5a339ad4a4eca3caa06dea284defc49729239c2
andrius <andrius.solopovas@gmail.com>
2024-12-16 00:46:27 +0000

    save

--- helpers/system/3-screen.sh
+++ helpers/system/3-screen.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 screens="eDP-1-1 HDMI-1-1 DP-1-1"
 pos_x=0
+cmd="xrandr"
 
 for screen in $screens; do
-    xrandr --output $screen --mode 1920x1080 --pos ${pos_x}x0 --rotate normal
+    cmd="$cmd --output $screen --mode 1920x1080 --pos ${pos_x}x0 --rotate normal"
     pos_x=$(($pos_x + 1920))
 done
+
+eval $cmd
