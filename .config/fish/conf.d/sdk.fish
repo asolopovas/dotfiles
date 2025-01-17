91603a0bda0c34474b233ea4f8044a9d2e84ec77
andrius <andrius.solopovas@gmail.com>
2025-01-17 11:13:49 +0000

    save

--- .config/fish/conf.d/sdk.fish
+++ .config/fish/conf.d/sdk.fish
@@ -13,7 +13,6 @@ if set -q __sdkman_custom_dir
 end
 # Guard: SDKMAN! needs to be installed
 if set -q SDKMAN_DIR; and not test -f "$SDKMAN_DIR/bin/sdkman-init.sh"
-    echo "WARNING: SDKMAN! installation path set to $SDKMAN_DIR, but no installation found there"
     exit 0
 end
 
