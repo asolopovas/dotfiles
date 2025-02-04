0234526a78d74bce99a432f80b90568e2e39978c
andrius <andrius.solopovas@gmail.com>
2025-02-04 10:06:57 +0000

    install-windows hello

--- scripts/install-windows-hello.sh
+++ scripts/install-windows-hello.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+curl -fsSLO https://github.com/evanphilip/WSL-Hello-sudo/releases/download/v2.1.1/release.tar.gz
+tar xvf release.tar.gz
+pushd ./release
+source install.sh
+
+rm -f release.tar.gz
+rm -rf release
+
+popd
