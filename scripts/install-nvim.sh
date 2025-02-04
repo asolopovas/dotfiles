2d2367c1df53f59ea2794257a36c567cec31c92e
andrius <andrius.solopovas@gmail.com>
2025-02-04 22:51:52 +0000

    save

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -4,21 +4,11 @@ source $HOME/dotfiles/globals.sh
 
 AUTOLOAD_DIR="$HOME/.local/share/nvim/site/autoload"
 
-print_color green "Installing Neovim for ${OS^} ..."
-
-if [ "$FORCE" = true ]; then
-    print_color red "FORCE Enabled: Removing ${AUTOLOAD_DIR} ..."
-    rm -rf $AUTOLOAD_DIR
+if ! cmd_exist nvim; then
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+    rm -rf /opt/nvim
+    tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 fi
-
-case $OS in
-ubuntu | debian | linuxmint | pop)
-    removePackage vim
-    if ! cmd_exist nvim; then
-        installPackages neovim python3-neovim
-    fi
-    ;;
-esac
 
 if [ ! -d "$AUTOLOAD_DIR" ]; then
     print_color green "Installing vim-plug ..."
@@ -27,5 +17,3 @@ if [ ! -d "$AUTOLOAD_DIR" ]; then
     bash -c "nvim +silent +PlugInstall +qall"
 fi
 
-ln -sf $(which nvim) $HOME/.local/bin
-ln -sf $(which nvim) $HOME/.local/bin/vim
