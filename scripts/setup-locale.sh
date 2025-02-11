b09b5311f89521f9e1a30086926d24d0f93024f3
andrius <andrius.solopovas@gmail.com>
2025-02-11 11:36:08 +0000

    save

--- scripts/setup-locale.sh
+++ scripts/setup-locale.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 setup_locale() {
-    LOCALE=${1:-en_US.UTF-8}
-    sudo sed -i "s/# $LOCALE UTF-8/$LOCALE UTF-8/" /etc/locale.gen
-    sudo locale-gen $LOCALE
-    sudo update-locale LC_ALL=$LOCALE LANG=$LOCALE
-    source ~/.bashrc
+    LOCALE=${1:-en_GB.UTF-8}
+    locale | grep -q "$LOCALE" && echo "$LOCALE already set." && return 0
+    sudo sed -i "/^# $LOCALE UTF-8/s/^# //" /etc/locale.gen
+    sudo locale-gen
+    sudo update-locale LC_ALL=$LOCALE LANG=$LOCALE LANGUAGE=$LOCALE
+    echo -e "LANG=$LOCALE\nLANGUAGE=$LOCALE\nLC_ALL=$LOCALE" | sudo tee /etc/default/locale > /dev/null
     echo "$LOCALE setup complete!"
 }
 
-setup_locale $1
+setup_locale "$1"
