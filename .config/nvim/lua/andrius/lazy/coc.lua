42f58bd084af75a61f8278d2e22d13ffb60fe65b
andrius <andrius.solopovas@gmail.com>
2025-02-17 02:51:07 +0000

    save

--- .config/nvim/lua/andrius/lazy/coc.lua
+++ .config/nvim/lua/andrius/lazy/coc.lua
@@ -2,7 +2,6 @@ return {
   {
     "neoclide/coc.nvim",
     branch = "release", -- Use the recommended release branch
-    build = "npm ci", -- Ensure dependencies are installed (only needed for master branch)
     cond = function() return vim.fn.executable("npm") == 1 end, -- Only install if npm is available
   },
 }
