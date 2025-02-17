2b6e210e55df982713b7c31746a2d98785a9bdeb
andrius <andrius.solopovas@gmail.com>
2025-02-17 09:01:09 +0000

    save

--- .config/nvim/lua/andrius/lazy/coc.lua
+++ .config/nvim/lua/andrius/lazy/coc.lua
@@ -1,24 +1,14 @@
 return {
-  {
-    "neoclide/coc.nvim",
-    branch = "release",
-    cond = function() return vim.fn.executable("npm") == 1 end, -- Ensure npm is available
-
-    config = function()
-      -- Define the path for a flag file to track installation
-      local install_flag = vim.fn.stdpath("data") .. "/coc_installed"
-
-      -- Function to install CoC extensions only if they are not already installed
-      local function install_coc_extensions()
-        if vim.fn.filereadable(install_flag) == 0 then
-          vim.cmd("CocInstall -sync coc-emmet coc-json coc-tsserver | q")
-          -- Create the flag file after installation
-          vim.fn.writefile({}, install_flag)
-        end
+   {
+      "neoclide/coc.nvim",
+      branch="release",
+    cond = function() return vim.fn.executable("npm") == 1 end, -- Only install if npm is available
+      config = function()
+         utils = require("utils")
+         utils.run_once("coc", function()
+            vim.cmd([[:CocInstall coc-json coc-yaml coc-markdownlint coc-explorer coc-sh coc-pyright]])
+            vim.cmd([[:CocInstall coc-lists coc-html coc-tsserver coc-go]])
+         end)
       end
-
-      -- Delay execution to ensure `coc.nvim` is fully loaded
-      vim.defer_fn(install_coc_extensions, 1000)
-    end
-  },
+   }
 }
