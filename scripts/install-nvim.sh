e01750437c2eba68d5d9aa8475521a5b19c5677b
andrius <andrius.solopovas@gmail.com>
2025-02-17 12:38:25 +0000

    save

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -9,5 +9,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm -f nvim-linux-x86_64.tar.gz
-ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/bin/vim
 
