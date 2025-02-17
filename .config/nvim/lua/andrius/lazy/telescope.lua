4c415e69e064b036fc173dcaa49510bcf1b924ec
andrius <andrius.solopovas@gmail.com>
2025-02-17 10:56:08 +0000

    save

--- .config/nvim/lua/andrius/lazy/telescope.lua
+++ .config/nvim/lua/andrius/lazy/telescope.lua
@@ -1,18 +1,38 @@
 return {
     "nvim-telescope/telescope.nvim",
-
     tag = "0.1.5",
-
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
-
     config = function()
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
+
+        -- Function to check if a .git directory exists in the current or parent directories
+        local function is_git_repo()
+            local dir = vim.fn.getcwd()  -- Get current working directory
+            while dir ~= "/" do
+                if vim.fn.isdirectory(dir .. "/.git") == 1 then
+                    return true
+                end
+                dir = vim.fn.fnamemodify(dir, ":h")  -- Move up one directory
+            end
+            return false
+        end
+
+        -- Smart file search: Git files if in a repo, otherwise find all files
+        local function smart_find_files()
+            if is_git_repo() then
+                builtin.git_files()
+            else
+                builtin.find_files()
+            end
+        end
+
+        -- Key mappings
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<C-p>', smart_find_files, {}) -- Super fast Git check
         vim.keymap.set('n', '<leader>pws', function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
@@ -27,4 +47,3 @@ return {
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
-
