f27b7097f694b79dcf787fc62c7ced60f7984e8d
andrius <andrius.solopovas@gmail.com>
2025-02-17 09:13:40 +0000

    save

--- .config/nvim/lua/andrius/lazy/coc.lua
+++ .config/nvim/lua/andrius/lazy/coc.lua
@@ -4,7 +4,7 @@ return {
       branch="release",
     cond = function() return vim.fn.executable("npm") == 1 end, -- Only install if npm is available
       config = function()
-         utils = require("utils")
+         utils = require("andrius.utils")
          utils.run_once("coc", function()
             vim.cmd([[:CocInstall coc-json coc-yaml coc-markdownlint coc-explorer coc-sh]])
             vim.cmd([[:CocInstall coc-lists coc-html coc-tsserver coc-go]])
