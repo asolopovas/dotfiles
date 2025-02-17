ba8112295a717c8b93494d4ba548b6c0143061e5
andrius <andrius.solopovas@gmail.com>
2025-02-17 02:41:45 +0000

    save

--- .config/nvim/lua/andrius/lazy/emmet.lua
+++ .config/nvim/lua/andrius/lazy/emmet.lua
@@ -1,18 +1,8 @@
-local lspconfig = require('lspconfig')
-local configs = require('lspconfig/configs')
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
-lspconfig.emmet_ls.setup({
-    -- on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-})
+return {
+  {
+    "neoclide/coc.nvim",
+    branch = "release", -- Use the recommended release branch
+    build = "npm ci", -- Ensure dependencies are installed (only needed for master branch)
+    cond = function() return vim.fn.executable("npm") == 1 end, -- Only install if npm is available
+  },
+}
