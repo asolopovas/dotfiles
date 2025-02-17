9f15f06f03a99f2b70f21ad6498f9ceee669e5de
andrius <andrius.solopovas@gmail.com>
2025-02-17 11:16:20 +0000

    save

--- .config/nvim/lua/andrius/init.lua
+++ .config/nvim/lua/andrius/init.lua
@@ -34,7 +34,7 @@ autocmd('TextYankPost', {
 autocmd('BufEnter', {
     group = AUGroup,
     callback = function()
-        vim.cmd.colorscheme("brightburn")
+        vim.cmd.colorscheme("legacy")
     end
 })
 
