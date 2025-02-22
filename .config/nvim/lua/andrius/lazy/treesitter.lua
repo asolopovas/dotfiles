860bdf449e44b07986db007404b2271a29859233
andrius <andrius.solopovas@gmail.com>
2025-02-22 00:34:01 +0000

    save

--- .config/nvim/lua/andrius/lazy/treesitter.lua
+++ .config/nvim/lua/andrius/lazy/treesitter.lua
@@ -5,8 +5,7 @@ return {
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
-                "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash", "php", "go"
+                "vimdoc", "javascript", "typescript", "lua", "jsdoc", "bash", "php", "fish"
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
