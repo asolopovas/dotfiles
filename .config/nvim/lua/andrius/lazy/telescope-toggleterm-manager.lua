329f4b4feca11c4f1583576d42f8a2e2fbe390e0
andrius <andrius.solopovas@gmail.com>
2025-03-01 20:55:27 +0000

    save

--- .config/nvim/lua/andrius/lazy/telescope-toggleterm-manager.lua
+++ .config/nvim/lua/andrius/lazy/telescope-toggleterm-manager.lua
@@ -1,9 +1,38 @@
 return {
-    "ryanmsnyder/toggleterm-manager.nvim",
-    dependencies = {
-        "akinsho/nvim-toggleterm.lua",
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim", -- only needed because it's a dependency of telescope
-    },
-    config = true,
+  "da-moon/telescope-toggleterm.nvim",
+  dependencies = {
+    "akinsho/toggleterm.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("telescope").load_extension("toggleterm")
+
+    -- ToggleTerm configuration
+    require("toggleterm").setup{
+      size = 20,
+      open_mapping = [[<leader>j]], -- Set <leader>j to toggle terminal
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      -- direction = "float", -- You can change to 'horizontal' or 'vertical'
+      close_on_exit = true,
+      shell = vim.o.shell,
+    }
+
+    -- Telescope-ToggleTerm setup
+    require("telescope-toggleterm").setup{
+      telescope_mappings = {
+        ["<C-c>"] = require("telescope-toggleterm").actions.exit_terminal,
+      },
+    }
+
+    -- Keymap for Telescope-ToggleTerm picker
+    vim.keymap.set("n", "<leader>tt", "<cmd>Telescope toggleterm<CR>", { noremap = true, silent = true })
+  end,
 }
+
--- .config/nvim/lua/andrius/lazy/telescope.lua
+++ .config/nvim/lua/andrius/lazy/telescope.lua
@@ -3,6 +3,10 @@ return {
     tag = "0.1.5",
     dependencies = {
         "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        },
     },
     config = function()
         require('telescope').setup({})
