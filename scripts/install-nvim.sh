d2bdf39284dbe94629fee938cbe715c8af1892d8
andrius <andrius.solopovas@gmail.com>
2025-06-04 22:57:35 +0100

    save

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
     echo "Please run as root"
     exit 1
 fi
 
-# Install Neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm -f nvim-linux-x86_64.tar.gz
 ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/bin/vim
 
-# Wait for Lazy.nvim setup to finish
 /opt/nvim-linux-x86_64/bin/nvim --headless +"autocmd User LazyDone ++once qa" +Lazy sync
-
-# Update Mason tools (if mason-tool-installer is used)
-/opt/nvim-linux-x86_64/bin/nvim --headless -c "lua require('mason-tool-installer').run_on_start()" -c 'qa'
