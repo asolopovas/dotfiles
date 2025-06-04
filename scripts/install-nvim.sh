dfeda178aac08d38f26b56c10881596efc8fc654
andrius <andrius.solopovas@gmail.com>
2025-06-04 23:12:51 +0100

    save

--- scripts/install-nvim.sh
+++ scripts/install-nvim.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+INSTALL_ARCHIVE=nvim-linux-x86_64.tar.gz
+URL="https://github.com/neovim/neovim/releases/latest/download/$INSTALL_ARCHIVE"
+
 if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run as root"
-    exit 1
+    DIR="$HOME/.local"
+    BIN="$DIR/bin"
+    mkdir -p "$BIN"
+    curl -LO "$URL"
+    tar -xzf "$INSTALL_ARCHIVE" -C "$DIR"
+    mv "$DIR/nvim-linux-x86_64" "$DIR/nvim"
+    ln -sf "$DIR/nvim/bin/nvim" "$BIN/vim"
+else
+    curl -LO "$URL"
+    rm -rf /opt/nvim
+    tar -C /opt -xzf "$INSTALL_ARCHIVE"
+    rm -f "$INSTALL_ARCHIVE"
+    ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/bin/vim
 fi
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-rm -rf /opt/nvim
-tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-rm -f nvim-linux-x86_64.tar.gz
-ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/bin/vim
+rm -f "$INSTALL_ARCHIVE"
