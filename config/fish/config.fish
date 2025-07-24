669b503eb6356fc6bb83ca067d11f45d609eae73
andrius <andrius.solopovas@gmail.com>
2025-07-24 20:40:35 +0100

    Add conda initialization and improve bash sourcing safety

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -67,3 +67,16 @@ set --export PATH $BUN_INSTALL/bin $PATH
 set -x LD_LIBRARY_PATH "/home/andrius/.rye/tools/rmodel/lib/python3.12/site-packages/nvidia/cudnn/lib:/home/andrius/.rye/tools/rmodel/lib/python3.12/site-packages/nvidia/cuda_runtime/lib:$LD_LIBRARY_PATH"
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -d "/home/andrius/miniconda3" -a -f "/home/andrius/miniconda3/bin/conda"
+    eval "/home/andrius/miniconda3/bin/conda" "shell.fish" "hook" 2> /dev/null | source
+else
+    if test -f "/home/andrius/miniconda3/etc/fish/conf.d/conda.fish"
+        source "/home/andrius/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/home/andrius/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
