d5859077d0bcc61f14f7119a6a1cb625a44e9395
andrius <andrius.solopovas@gmail.com>
2025-07-28 10:48:57 +0100

    active desktop retained

--- scripts/polybar-xmonad.sh
+++ scripts/polybar-xmonad.sh
@@ -4,16 +4,23 @@
 # Shows all screens with all desktops 1-8, highlighting active ones
 # The active workspace is highlighted with %{F#2266d0} in xmonad-log output
 
+# Track active desktops for each screen
+declare -A screen_active_desktop
+
 $HOME/go/bin/xmonad-log | while IFS= read -r line; do
-    # Extract active screen and desktop from highlighted workspace
-    active_screen=""
-    active_desktop=""
-    if [[ $line =~ %\{F#2266d0\}\ ([0-9]+)_([0-9]+)\ %\{F-\} ]]; then
-        active_screen="${BASH_REMATCH[1]}"
-        active_desktop="${BASH_REMATCH[2]}"
-    fi
+    # Extract all screen-desktop pairs from the line and find active workspaces
+    temp_line="$line"
     
-    # Extract all screens mentioned in the line
+    # Parse all workspaces and identify active ones
+    while [[ $temp_line =~ ([^%]*)%\{F#[0-9a-fA-F]+\}\ ([0-9]+)_([0-9]+)\ %\{F-\}(.*) ]] || [[ $temp_line =~ ([^%]*)%\{F#2266d0\}\ ([0-9]+)_([0-9]+)\ %\{F-\}(.*) ]]; do
+        screen_id="${BASH_REMATCH[2]}"
+        desktop_id="${BASH_REMATCH[3]}"
+        # Update active desktop for this screen
+        screen_active_desktop[$screen_id]=$desktop_id
+        temp_line="${BASH_REMATCH[1]}${BASH_REMATCH[4]}"
+    done
+    
+    # Extract all screens mentioned in the original line
     screens=()
     temp_line="$line"
     while [[ $temp_line =~ ([0-9]+)_([0-9]+) ]]; do
@@ -39,7 +46,7 @@ $HOME/go/bin/xmonad-log | while IFS= read -r line; do
         
         # Show desktops 1-8 for this screen
         for desktop in {1..8}; do
-            if [[ "$screen" == "$active_screen" && "$desktop" == "$active_desktop" ]]; then
+            if [[ "${screen_active_desktop[$screen]}" == "$desktop" ]]; then
                 # Highlight active desktop with #ffe500 color
                 display+="%{F#ffe500}$desktop%{F-}"
             else
@@ -53,7 +60,7 @@ $HOME/go/bin/xmonad-log | while IFS= read -r line; do
         
         # Add separator between screens if not last
         if [[ $i -lt $((${#sorted_screens[@]} - 1)) ]]; then
-            display+="  |  "
+            display+=" | "
         fi
     done
     
