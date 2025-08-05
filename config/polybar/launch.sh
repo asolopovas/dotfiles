a8f0c7784fd00d6ee9ccf0780b5a262768961bd5
andrius <andrius.solopovas@gmail.com>
2025-08-05 20:03:51 +0100

    save

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,10 +4,8 @@ CONFIG_DIR=$HOME/dotfiles/config/polybar
 THEME=minimal
 CONFIG=$CONFIG_DIR/themes/$THEME/config.ini
 
-polybar main -c $CONFIG_DIR &
-
 killall polybar >/dev/null 2>&1
-while pgrep -u $UID -x polybar >$HOME/logs/polybar.log; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 net_interface=$(ip route show | grep default | awk '{print$5}')
 
