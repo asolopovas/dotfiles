c7cf48e499cecfcf2f3330fd2c5ac64e80fa22d1
andrius <andrius.solopovas@gmail.com>
2026-02-06 20:26:16 +0000

    fix: prevent OSC 11 background color query leaking as visible text

--- .config/nvim/lua/andrius/set.lua
+++ .config/nvim/lua/andrius/set.lua
@@ -17,6 +17,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
+vim.opt.background = "dark"
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
