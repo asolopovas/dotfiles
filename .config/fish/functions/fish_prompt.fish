ca5ad9828de868472f0b95ab97f335d494bfb1d8
andrius <andrius.solopovas@gmail.com>
2026-02-06 20:34:36 +0000

    fix: inline set_color calls in fish_prompt to prevent OSC 11 query leak

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,15 +1,6 @@
 # name: default
 # ---------------
 
-# Initialize color settings at the start of the session.
-set -U color_cyan (set_color cyan)
-set -U color_yellow (set_color yellow)
-set -U color_red (set_color red)
-set -U color_blue (set_color blue)
-set -U color_green (set_color green)
-set -U color_brgreen (set_color brgreen)
-set -U color_normal (set_color normal)
-
 function _git_info
     set -l branch (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
     if [ "$branch" ]
@@ -31,41 +22,41 @@ end
 function fish_prompt
     set -l last_status $status
     set -l user (whoami)
-    set -l cwd $color_blue(pwd | sed "s:^$HOME:~:")
+    set -l cwd (set_color blue)(pwd | sed "s:^$HOME:~:")
 
     # Show SSH hostname if connected via SSH
     set -l sshinfo (_ssh_info)
     if [ "$sshinfo" ]
-        echo -n -s $color_cyan $sshinfo $color_normal ' '
+        echo -n -s (set_color cyan) $sshinfo (set_color normal) ' '
     end
 
     # Display [venvname] if in a virtualenv
     if set -q VIRTUAL_ENV
-        echo -n -s $color_cyan '[' (basename "$VIRTUAL_ENV") ']' $color_normal ' '
+        echo -n -s (set_color cyan) '[' (basename "$VIRTUAL_ENV") ']' (set_color normal) ' '
     end
 
     # Print pwd or full path
-    echo -n -s $cwd $color_normal
+    echo -n -s $cwd (set_color normal)
 
     # Show git branch and status
     set -l gitinfo (_git_info)
     if [ "$gitinfo" ]
         if echo $gitinfo | grep "±" > /dev/null
-            echo -n -s ' ' $color_yellow $gitinfo $color_normal
+            echo -n -s ' ' (set_color yellow) $gitinfo (set_color normal)
         else
-            echo -n -s ' ' $color_green $gitinfo $color_normal
+            echo -n -s ' ' (set_color green) $gitinfo (set_color normal)
         end
     end
 
     # Display username
-    echo -n -s $color_yellow ' [ ' $color_brgreen  $user $color_yellow ' ]' $color_normal
+    echo -n -s (set_color yellow) ' [ ' (set_color brgreen) $user (set_color yellow) ' ]' (set_color normal)
 
-    set -l prompt_color $color_red
+    set -l prompt_color (set_color red)
     if test $last_status = 0
-        set prompt_color $color_normal
+        set prompt_color (set_color normal)
     end
 
     # Terminate with a nice prompt char
     echo -e ''
-    echo -e -n -s $prompt_color '> ' $color_normal
+    echo -e -n -s $prompt_color '> ' (set_color normal)
 end
