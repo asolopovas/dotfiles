fc26c790ab73139fcd8076e7de9bce28002e68bf
andrius <andrius.solopovas@gmail.com>
2026-04-12 16:19:03 +0100

    Lazy-load sg.nvim: skip when Sourcegraph env vars are missing

--- .config/nvim/lua/andrius/lazy/cody.lua
+++ .config/nvim/lua/andrius/lazy/cody.lua
@@ -2,6 +2,10 @@ return {
   {
     "sourcegraph/sg.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "CodyChat", "CodyToggle", "SourcegraphSearch" },
+    cond = function()
+      return vim.env.SRC_ENDPOINT ~= nil and vim.env.SRC_ACCESS_TOKEN ~= nil
+    end,
   },
 }
 
