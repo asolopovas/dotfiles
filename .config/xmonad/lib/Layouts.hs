d3170c44e4ee166575f73d3840c7edfc1876f4ac
andrius <andrius.solopovas@gmail.com>
2026-05-10 13:16:55 +0100

    fix(xmonad): only center single window when it is a terminal
    Replace pure CenterMid layout with class-aware CenterMidIf that consults
    className via doLayout. Centers at 70%x80% only for terminal classes
    (Alacritty, kitty, st, URxvt, XTerm); other single-window apps (browsers,
    file managers, etc.) now use the full screen rectangle as expected.

--- .config/xmonad/lib/Layouts.hs
+++ .config/xmonad/lib/Layouts.hs
@@ -20,18 +20,31 @@ import XMonad.Layout.Spacing (Border (..), Spacing, spacingRaw)
 import qualified XMonad.Layout.ToggleLayouts as T
 import qualified XMonad.StackSet as W
 
-data CenterMid a = CenterMid Rational Rational deriving (Show, Read)
+terminalClasses :: [String]
+terminalClasses = ["Alacritty", "kitty", "Kitty", "st-256color", "URxvt", "XTerm"]
 
-instance LayoutClass CenterMid Window where
-    pureLayout (CenterMid wr hr) (Rectangle sx sy sw sh) stack =
+centeredInside :: Rational -> Rational -> Rectangle -> Rectangle
+centeredInside wr hr (Rectangle sx sy sw sh) =
+    let w = floor (fromIntegral sw * wr :: Rational)
+        h = floor (fromIntegral sh * hr :: Rational)
+        x = sx + fromIntegral ((fromIntegral sw - w) `div` 2)
+        y = sy + fromIntegral ((fromIntegral sh - h) `div` 2)
+    in  Rectangle x y (fromIntegral w) (fromIntegral h)
+
+data CenterMidIf a = CenterMidIf [String] Rational Rational deriving (Show, Read)
+
+instance LayoutClass CenterMidIf Window where
+    doLayout (CenterMidIf classes wr hr) screenRect stack = do
         let ws = W.integrate stack
-            w  = floor (fromIntegral sw * wr :: Rational)
-            h  = floor (fromIntegral sh * hr :: Rational)
-            x  = sx + fromIntegral ((fromIntegral sw - w) `div` 2)
-            y  = sy + fromIntegral ((fromIntegral sh - h) `div` 2)
-            r  = Rectangle x y (fromIntegral w) (fromIntegral h)
-        in  zip ws (repeat r)
-    description _ = "CenterMid"
+        case ws of
+            [w] -> do
+                cls <- runQuery className w
+                let r = if cls `elem` classes
+                            then centeredInside wr hr screenRect
+                            else screenRect
+                pure ([(w, r)], Nothing)
+            _ -> pure (zip ws (repeat screenRect), Nothing)
+    description _ = "CenterMidIf"
 
 evenSpacing :: Integer -> l a -> LM.ModifiedLayout Spacing l a
 evenSpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
@@ -45,7 +58,7 @@ tiled =
         $ smartBorders
         $ limitWindows 12
         $ evenSpacing 5
-        $ ifMax 1 (CenterMid (7 / 10) (4 / 5)) baseTile
+        $ ifMax 1 (CenterMidIf terminalClasses (7 / 10) (4 / 5)) baseTile
 
 tiledR =
     renamed [Replace "tiledR"]
@@ -54,7 +67,7 @@ tiledR =
         $ limitWindows 12
         $ evenSpacing 5
         $ reflectHoriz
-        $ ifMax 1 (CenterMid (7 / 10) (4 / 5)) baseTile
+        $ ifMax 1 (CenterMidIf terminalClasses (7 / 10) (4 / 5)) baseTile
 
 full =
     renamed [Replace "full"] $
