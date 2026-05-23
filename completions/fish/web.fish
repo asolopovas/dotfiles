897de536cba6baad7b20d52d35903dee2e541ade
andrius <andrius.solopovas@gmail.com>
2026-05-23 20:58:40 +0100

    use fzf for sysact

--- completions/fish/web.fish
+++ completions/fish/web.fish
@@ -1,21 +1,25 @@
+
 function __web_debug
     set -l file "$BASH_COMP_DEBUG_FILE"
     if test -n "$file"
-        echo "$argv" >>$file
+        echo "$argv" >> $file
     end
 end
 
 function __web_perform_completion
     __web_debug "Starting __web_perform_completion"
+
     set -l args (commandline -opc)
     set -l lastArg (string escape -- (commandline -ct))
 
     __web_debug "args: $args"
     __web_debug "last arg: $lastArg"
+
     set -l requestComp "WEB_ACTIVE_HELP=0 $args[1] __complete $args[2..-1] $lastArg"
 
     __web_debug "Calling $requestComp"
     set -l results (eval $requestComp 2> /dev/null)
+
     for line in $results[-1..1]
         if test (string trim -- $line) = ""
             set results $results[1..-2]
@@ -26,6 +30,7 @@ function __web_perform_completion
 
     set -l comps $results[1..-2]
     set -l directiveLine $results[-1]
+
     set -l flagPrefix (string match -r -- '-.*=' "$lastArg")
 
     __web_debug "Comps: $comps"
@@ -38,6 +43,7 @@ function __web_perform_completion
 
     printf "%s\n" "$directiveLine"
 end
+
 function __web_perform_completion_once
     __web_debug "Starting __web_perform_completion_once"
 
@@ -55,6 +61,7 @@ function __web_perform_completion_once
     __web_debug "Performed completions and set __web_perform_completion_once_result"
     return 0
 end
+
 function __web_clear_perform_completion_once_result
     __web_debug ""
     __web_debug "========= clearing previously set __web_perform_completion_once_result variable =========="
@@ -87,9 +94,12 @@ function __web_requires_order_preservation
     __web_debug "This doesn't require order preservation"
     return 1
 end
+
+
 function __web_prepare_completions
     __web_debug ""
     __web_debug "========= starting completion logic =========="
+
     set --erase __web_comp_results
 
     __web_perform_completion_once
@@ -133,6 +143,7 @@ function __web_prepare_completions
     set -l nofiles (math (math --scale 0 $directive / $shellCompDirectiveNoFileComp) % 2)
 
     __web_debug "nospace: $nospace, nofiles: $nofiles"
+
     if test $nospace -ne 0; or test $nofiles -eq 0
         set -l prefix (commandline -t | string escape --style=regex)
         __web_debug "prefix: $prefix"
@@ -140,11 +151,13 @@ function __web_prepare_completions
         set -l completions (string match -r -- "^$prefix.*" $__web_comp_results)
         set --global __web_comp_results $completions
         __web_debug "Filtered completions are: $__web_comp_results"
+
         set -l numComps (count $__web_comp_results)
         __web_debug "numComps: $numComps"
 
         if test $numComps -eq 1; and test $nospace -ne 0
             set -l split (string split --max 1 \t $__web_comp_results[1])
+
             set -l lastChar (string sub -s -1 -- $split)
             if not string match -r -q "[@=/:.,]" -- "$lastChar"
                 __web_debug "Adding second completion to perform nospace directive"
@@ -161,10 +174,13 @@ function __web_prepare_completions
 
     return 0
 end
-if type -q web
-    complete --do-complete "web " >/dev/null 2>&1
+
+if type -q "web"
+    complete --do-complete "web " > /dev/null 2>&1
 end
+
 complete -c web -e
-complete -c web -n __web_clear_perform_completion_once_result
+
+complete -c web -n '__web_clear_perform_completion_once_result'
 complete -c web -n 'not __web_requires_order_preservation && __web_prepare_completions' -f -a '$__web_comp_results'
 complete -k -c web -n '__web_requires_order_preservation && __web_prepare_completions' -f -a '$__web_comp_results'
